4a17f0aebfd947ca3cc153f27c36da8dc5e3a93b
Nathan Smith <nathan.smith12@nhs.net>
2022-02-28 23:39:20 +0000

    Create base template and static files

--- asset_management_app/urls.py
+++ asset_management_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from asset_management_app import views
+import asset_management_app
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("hello/<name>", views.hello_there, name="hello_there"),
 ]
--- asset_management_app/views.py
+++ asset_management_app/views.py
@@ -1,4 +1,18 @@
+import re
+from django.shortcuts import render
+from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+
+def hello_there(request, name):
+    return render(
+        request,
+        'asset_management_app/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
